1a9c5bbc20b7f3820c93c8fde8cd32a9502d72e3
Amr Adel <161164138+AmrAdel2611@users.noreply.github.com>
2024-08-01 04:36:06 +0300

    Update login.js

--- login.js
+++ login.js
@@ -5,7 +5,7 @@ document.getElementById('loginForm').addEventListener('submit', function (event)
 
     if (username === 'user' && password === '123') {
         alert('Login successful!');
-        window.location.href = 'index.html'; // Redirect to index.html
+        window.location.href = 'tracker.html'; // Redirect to index.html
     } else {
         alert('Invalid username or password.');
     }
